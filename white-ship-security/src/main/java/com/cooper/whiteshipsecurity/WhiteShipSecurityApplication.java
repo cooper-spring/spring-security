961343b6efa98e95dcecca041f2dbe50c9b88acb
cooper <pbg0205@naver.com>
2022-05-24 22:40:05 +0900

    white-ship-security first commit

---
+++ white-ship-security/src/main/java/com/cooper/whiteshipsecurity/WhiteShipSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cooper.whiteshipsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteShipSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteShipSecurityApplication.class, args);
+    }
+
+}
---
+++ white-ship-security/src/test/java/com/cooper/whiteshipsecurity/WhiteShipSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.whiteshipsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteShipSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
